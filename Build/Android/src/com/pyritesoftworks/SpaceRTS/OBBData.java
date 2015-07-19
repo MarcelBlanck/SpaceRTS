40777abdc0f5f3a8cb81f5f209ac64a6954375dc
Marcel Blanck <mail@marcel-blanck.de>
2015-07-19 13:51:15 +0200

    Snapshot MainMenu is working, true reason unknown

--- Build/Android/src/com/pyritesoftworks/SpaceRTS/OBBData.java
+++ Build/Android/src/com/pyritesoftworks/SpaceRTS/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-99826316 // the length of the file in bytes
+99825630 // the length of the file in bytes
 )
 };
 };
